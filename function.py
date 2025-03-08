5176d4aa1eb6728e04d9368fb5468d4be05682e3
Fr33zln <fre3ezln@gmail.com>
2025-03-08 11:50:58 +0200

    Update function.py

--- function.py
+++ function.py
@@ -12,6 +12,9 @@ class Human(pygame.Rect):
         self.image_now = self.image
         self.image_count = 0
         self.step = step
+
+
+
     def move_image(self):
         if self.image_count == len(self.image_list * 10) - 1:
             self.image_count = 0
@@ -20,19 +23,47 @@ class Human(pygame.Rect):
         self.image_count += 1
 
 class Hero(Human):
-    def __init__(self, x, y, width, height, image_list, step,hp):
-        super().__init__(x, y, width, height, image_list, step)
-        self.walk = {"up": False, "down":False,"right": False, "left": False}
+    def __init__(self,x,y,width,height,image_list,step):
+        super().__init__(x,y,width,height,image_list,step)
+        self.walk = {"up": False, "down": False, "left":False,"right":False}
         self.side = False
+        self.hp = hp
         
 
     def move(self, window):
         if self.walk["up"] and self.y > 0:
-            self.y -=self.step
+            self.y -= self.step
+            if self.collidelist(wall_list) != -1:
+                self.y += self.step
+        if self.walk["down"] and self.y < size_window[1]:
+            self.y += self.step
+            if self.collidelist(wall_list) != -1:
+                self.y -= self.step
+        if self.walk["left"] and self.x > 0:
+            self.x -= self.step
+            if self.collidelist(wall_list) != -1:
+                self.x += self.step
+            self.side = True
+        if self.walk["right"] and self.x < size_window[0]:
+            self.x += self.step
+            if self.collidelist(wall_list) != -1:
+                self.x -= self.step
+            self.side = False
 
+        for value in list(self.walk.values()):
+            if value:
+                self.move_image()
+                break
+        else:
+            self.image = self.image_list[0]
+        if self.side:
+            self.image_now = pygame.transform.flip(self.image, True , False)
+        else:
+            self.image_now = self.image
 
         self.move_image()
-        window.blit(self.image, (self.x, self.y))
+        window.blit(self.image_now, (self.x,self.y))
+
 
 
 
@@ -59,7 +90,33 @@ class Bot(Human):
         self.move_image()
         window.blit(self.image,(self.x,self.y))
 
-    #def atacker
+
+
+    def striker(self,window,bullet):
+        self.move_image()
+        window.blit
+
+class Bullet(pygame.Rect):
+    def __init__(self,x,y,width,height,color,orientation,step,image = None):
+        super().__init__(x,y,width,height)
+        self.color = color
+        self.image = image
+        self.orientation = orientation
+        self.start_x = x
+        self.start_y = y
+        self.step = step
+
+    def move(self, window):
+        if self.orientation == 'vertical':
+            self.y += self.step
+            if self.y < 0 or self.y > size_window[1] or self.collidelist(wall_list) != -1:
+                self.y = self.start_y
+        elif self.orientation == 'horizontal':
+            self.x += self.step
+            if self.x < 0 or self.x > size_window[1] or self.collidelist(wall_list) != -1:
+                self.x = self.start_x
+        pygame.draw.rect(window,self.color,self)
+
 
 
 class Wall(pygame.Rect):
@@ -79,4 +136,3 @@ def create_wall(new_map):
         y += height
 
 create_wall(maps["LVL1"]["map"])
-#print(len(wall_list))
